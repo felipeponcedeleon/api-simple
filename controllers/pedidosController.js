7e08016f3be70c3c1ecec1794f0289d6a6f5cd30
felipeponcedeleonespinosa <59258794+felipeponcedeleonespinosa@users.noreply.github.com>
2020-03-05 15:49:14 -0400

    Sección Pedidos - Mostrar, actualizar y eliminar por ID

--- controllers/pedidosController.js
+++ controllers/pedidosController.js
@@ -26,12 +26,40 @@ exports.mostrarPedidos = async(req, res, next) => {
 
 exports.mostrarPedido = async(req, res, next) => {
 
+    const pedido = await Pedidos.findById(req.params.idPedido).populate('cliente').populate({
+            path: 'pedido.producto',
+            model: 'Productos'
+        });
+
+    if(!pedido) {
+        res.json({mensaje: 'El pedido no existe.'});
+        return next();
+    }
+
+    res.json(pedido);
 }
 
 exports.actualizarPedido = async(req, res, next) => {
+
+    try {
+        let pedido = await Pedidos.findByIdAndUpdate({_id: req.params.idPedido}, req.body, {
+            new: true
+        });
+        res.json(pedido);
+    } catch(error){
+        console.log(error);
+        next();
+    }
 
 }
 
 exports.eliminarPedido = async(req, res, next) => {
 
+    try {
+        await Pedidos.findOneAndDelete({_id: req.params.idPedido});
+        res.json({mensaje: 'El pedido se ha eliminado.'});
+    } catch(error){
+        console.log(error);
+        next();
+    }
 }
