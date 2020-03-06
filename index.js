19f02e2314104859a7429a342dae5818cbe8f987
felipeponcedeleonespinosa <59258794+felipeponcedeleonespinosa@users.noreply.github.com>
2020-03-06 01:32:14 -0400

    CORS

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+//CORS permite que un cliente se conecte a otro servidor para el intercambio
+//de recuersos
+const cors = require('cors');
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/restapidb', {
     useNewUrlParser: true,
@@ -14,6 +18,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//configurar cors
+app.use(cors());
+
 app.use('/', routes());
 
-app.listen(3000);
+app.listen(5000);
