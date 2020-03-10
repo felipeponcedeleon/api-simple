9402c35f9c70d48e232429eaa6e88f1bb522d401
felipeponcedeleonespinosa <59258794+felipeponcedeleonespinosa@users.noreply.github.com>
2020-03-10 17:47:57 -0400

    Controlador Usuarios - Crear usuario (SignUp)

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,8 +1,19 @@
-exports.registrarUsuario = () => {
+const Usuarios = require('../models/Usuarios');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
+exports.registrarUsuario = async(req, res) => {
+    const usuario = new Usuarios(req.body);
+    usuario.password = await bcrypt.hash(req.body.password, 10);
 
+    try {
+        await usuario.save();
+        res.json({ mensaje: 'Usuario creado con éxito!'});
+    } catch (error) {
+        res.json({ mensaje: 'Ocurrió un error.'});
+    }
 }
 
 exports.autenticarUsuario = () => {
-    
+
 }
